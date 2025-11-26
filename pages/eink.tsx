02746b324ce2634a712d8e6c6454905f62e98a3d
Manuel Reale <manuel.reale@mail.polimi.it>
2025-11-26 09:22:29 +0100

    hasd

--- pages/eink.tsx
+++ pages/eink.tsx
@@ -3,19 +3,6 @@ import React from "react";
 import Head from "next/head";
 import { pixelPatternStyles } from "../lib/pixelPatterns";
 
-const CORNER_VARIANTS = {
-  large: {
-    wrapper: "pixel-corners--wrapper",
-    inner: "pixel-corners",
-  },
-  small: {
-    wrapper: "pixel-corners-5--wrapper",
-    inner: "pixel-corners-5",
-  },
-} as const;
-
-type CornerVariant = keyof typeof CORNER_VARIANTS;
-
 export default function EInkCalendar() {
   const sprinklePattern = React.useMemo<React.CSSProperties>(
     () => ({
@@ -33,9 +20,6 @@ export default function EInkCalendar() {
     []
   );
 
-  const cornerVariant: CornerVariant = "large";
-  const { wrapper, inner } = CORNER_VARIANTS[cornerVariant];
-
   return (
     <>
       <Head>
@@ -46,20 +30,20 @@ export default function EInkCalendar() {
       <div className="w-full h-full absolute top-0 left-0">
         {/* <img src="/img2.png" className="w-full h-full absolute top-0 left-0 z-10" /> */}
         <div
-          className={`${wrapper} absolute inset-0`}
+          className="pixel-corners-10px--wrapper absolute inset-0"
           style={{ width: "100%", height: "100%" }}
         >
           <div
-            className={`${inner} w-full h-full relative`}
+            className="pixel-corners-10px w-full h-full relative"
             style={sprinklePattern}
           >
             <div className="flex items-center justify-center w-full h-full">
               <div
-                className="pixel-corners-5--wrapper"
+                className="pixel-corners-5px--wrapper"
                 style={{ width: "60%", maxWidth: 400 }}
               >
                 <div
-                  className="pixel-corners-5 w-full h-full p-6"
+                  className="pixel-corners-5px w-full h-full p-6"
                   style={checkerPattern}
                 >
                   <p className="text-sm tracking-wide uppercase">
