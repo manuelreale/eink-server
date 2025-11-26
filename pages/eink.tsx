c03e73072e99f9116f7ee5ca2d4ab121f3a9e70d
Manuel Reale <manuel.reale@mail.polimi.it>
2025-11-26 09:03:51 +0100

    corners

--- pages/eink.tsx
+++ pages/eink.tsx
@@ -4,6 +4,14 @@ import Head from "next/head";
 import { pixelPatternStyles } from "../lib/pixelPatterns";
 
 export default function EInkCalendar() {
+  const sprinklePattern = React.useMemo<React.CSSProperties>(
+    () => ({
+      ...pixelPatternStyles.sprinkle,
+      "--pixel-corner-fill": "#f5f5f5",
+    }),
+    []
+  );
+
   return (
     <>
       <Head>
@@ -13,7 +21,15 @@ export default function EInkCalendar() {
 
       <div className="w-full h-full absolute top-0 left-0">
         {/* <img src="/img2.png" className="w-full h-full absolute top-0 left-0 z-10" /> */}
-        <div className="w-full h-full absolute top-0 left-0" style={pixelPatternStyles.sprinkle}></div>
+        <div
+          className="pixel-corners--wrapper absolute inset-0"
+          style={{ width: "100%", height: "100%" }}
+        >
+          <div
+            className="pixel-corners w-full h-full"
+            style={sprinklePattern}
+          ></div>
+        </div>
       </div>
     </>
   );
