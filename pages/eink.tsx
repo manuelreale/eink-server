e62d63ffbe53829598cbf2ba9adc6b5d75555117
Manuel Reale <manuel.reale@mail.polimi.it>
2025-11-26 09:11:19 +0100

    test

--- pages/eink.tsx
+++ pages/eink.tsx
@@ -3,6 +3,19 @@ import React from "react";
 import Head from "next/head";
 import { pixelPatternStyles } from "../lib/pixelPatterns";
 
+const CORNER_VARIANTS = {
+  large: {
+    wrapper: "pixel-corners--wrapper",
+    inner: "pixel-corners",
+  },
+  small: {
+    wrapper: "pixel-corners-5--wrapper",
+    inner: "pixel-corners-5",
+  },
+} as const;
+
+type CornerVariant = keyof typeof CORNER_VARIANTS;
+
 export default function EInkCalendar() {
   const sprinklePattern = React.useMemo<React.CSSProperties>(
     () => ({
@@ -11,6 +24,9 @@ export default function EInkCalendar() {
     }),
     []
   );
+
+  const cornerVariant: CornerVariant = "large";
+  const { wrapper, inner } = CORNER_VARIANTS[cornerVariant];
 
   return (
     <>
@@ -22,11 +38,11 @@ export default function EInkCalendar() {
       <div className="w-full h-full absolute top-0 left-0">
         {/* <img src="/img2.png" className="w-full h-full absolute top-0 left-0 z-10" /> */}
         <div
-          className="pixel-corners--wrapper absolute inset-0"
+          className={`${wrapper} absolute inset-0`}
           style={{ width: "100%", height: "100%" }}
         >
           <div
-            className="pixel-corners w-full h-full"
+            className={`${inner} w-full h-full`}
             style={sprinklePattern}
           ></div>
         </div>
