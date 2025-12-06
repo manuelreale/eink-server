c20ef443c5de3a759630d7a1522c54511c956ec3
Manuel Reale <manuel.reale@mail.polimi.it>
2025-12-06 22:58:11 +0100

    Update up.tsx

--- pages/up.tsx
+++ pages/up.tsx
@@ -27,17 +27,18 @@ export default function EInkCalendar() {
       </Head>
 
       <div className="w-[960px] h-[680px] absolute top-0 left-0 flex flex-row">
-        <img src="/top2.png" className="w-full h-full absolute top-0 left-0 z-10 opacity-10" style={{ imageRendering: "pixelated" }}/>
+        <img src="/top2.png" className="w-full h-full absolute top-0 left-0 z-10 opacity-0" style={{ imageRendering: "pixelated" }}/>
         <div className="pixel-corners-10px w-1/4 h-full relative p-[16px] gap-[16px] flex flex-col items-center justify-top">
           <div className="text-center font-noto-sans-jp leading-[1.02] font-bold text-[91px] w-[60%] h-[273px]">
             土曜日
           </div>
-          <div className="text-center font-jersey25 text-[41px] h-[41px]">
+          <div className="text-center font-jersey25 text-[41px] h-[41px] w-[148px] leading-[41px] pl-[1px]">
             Sunday
           </div>
           <div className="text-left font-jacquarda-bastarda-9 text-[13px] w-[148px] h-[150px] leading-[17px] mt-[8px]">
           December 1 is the 335th day of the year (336th in leap years) in the Gregorian calendar; 30 days remain until the end of the year.
           </div>
+          <div className="pixel-corners-10px w-[160px] h-full my-[8px]"></div>
         </div>
         <div className="pixel-corners-10px w-1/2 h-full relative p-8"></div>
         <div className="pixel-corners-10px w-1/4 h-full relative p-8"></div>
